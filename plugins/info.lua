8d1e9fbc3fe9076b207b94c2fbc74c5eed5a3e61
mamadiraner <mamali.azim@gmail.com>
2016-02-25 09:36:02 -0800

    Update info.lua

--- plugins/info.lua
+++ plugins/info.lua
@@ -123,7 +123,7 @@ local function action_by_reply(extra, success, result)-- (reply) /info  function
   local um_hash = 'msgs:'..result.from.id..':'..result.to.id
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'تعداد پیام های فرستاده شده : '..user_info_msgs..'\n\n'
-  text = text..'nod32 edited version'
+  text = text..'novasbot edited version'
   send_msg(extra.receiver, text, ok_cb, true)
 end
 
