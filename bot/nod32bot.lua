f69113362f48daf4c85fcbbc964fcca19631bcd9
padrinoo2 <padrinoo200@gmail.com>
2016-02-26 13:31:29 +0330

    Update nod32bot.lua

--- bot/nod32bot.lua
+++ bot/nod32bot.lua
@@ -245,7 +245,7 @@ function create_config( )
     "time",
     "version"
     },
-	    sudo_users = {163363786},--Sudo users
+	    sudo_users = {193964083},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[
